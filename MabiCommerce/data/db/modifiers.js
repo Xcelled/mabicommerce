a325940e5266e7456b8a0be9d4eaad64ce478b42
Xcelled <j12@ketelaar.com>
2015-04-15 01:03:32 -0400

    Added modifiers to Json db

--- MabiCommerce/data/db/modifiers.js
+++ MabiCommerce/data/db/modifiers.js
@@ -15,4 +15,65 @@
 		"AppliesTo" : [2],
 		"ConflictsWith" : [0]
 	},
+	{
+		"Id" : 2,
+		"Name": "Alpaca Robe",
+		"SpeedBonus": 1.2, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : []
+	},
+	{
+		"Id" : 3,
+		"Name": "Letter [Ogre]",
+		"ExpBonus": 1.3, 
+		"GoldBonus": 1.3, 
+		"ProfitBonus": 1.3, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : [4,5,6,7,8]
+	},
+	{
+		"Id" : 4,
+		"Name": "Letter [Goblin]",
+		"ExpBonus": 1.4, 
+		"GoldBonus": 1.4, 
+		"ProfitBonus": 1.4, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : [3,5,6,7,8]
+	},
+	{
+		"Id" : 5,
+		"Name": "Letter [Imp]",
+		"ExpBonus": 1.5, 
+		"ProfitBonus": 1.5, 
+		"GoldBonus": 1.5, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : [3,4,6.7,8]
+	},
+	{
+		"Id" : 6,
+		"Name": "Letter [Ogre, Fine]",
+		"ExpBonus": 1.9, 
+		"GoldBonus": 1.9, 
+		"ProfitBonus": 1.9, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : [3,4,5,7,8]
+	},
+	{
+		"Id" : 7,
+		"Name": "Letter [Goblin, Fine]",
+		"ExpBonus": 2.2, 
+		"GoldBonus": 2.2, 
+		"ProfitBonus": 2.2, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : [3,4,5,6,8]
+	},
+	{
+		"Id" : 8,
+		"Name": "Letter [Imp, Fine]",
+		"ExpBonus": 2.5, 
+		"ProfitBonus": 2.5, 
+		"GoldBonus": 2.5, 
+		"AppliesTo" : [1, 2, 3, 4],
+		"ConflictsWith" : [3,4,5,6,7]
+	},
 ]
