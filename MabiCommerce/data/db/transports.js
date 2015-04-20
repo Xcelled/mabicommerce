1ea819ee8ad84ba46e6f399b48bd00c5454bbbe1
Xcelled <j12@ketelaar.com>
2015-04-20 17:22:57 -0400

    Fixed data error

--- MabiCommerce/data/db/transports.js
+++ MabiCommerce/data/db/transports.js
@@ -3,8 +3,8 @@
     "Name": "Backpack",
     "Icon": "data/img/transport/backpack.png",
     "SpeedFactor": 0.95,
-    "BaseSlots": 4,
-    "BaseWeight": 400,
+    "Slots": 4,
+    "Weight": 400,
     "IsRequired": true,
     "Id": 0
   },
@@ -12,24 +12,24 @@
     "Name": "Handcart",
     "Icon": "data/img/transport/handcart.png",
     "SpeedFactor": 1.0,
-    "BaseSlots": 6,
-    "BaseWeight": 800,
+    "Slots": 6,
+    "Weight": 800,
     "Id": 1
   },
   {
     "Name": "Wagon",
     "Icon": "data/img/transport/wagon.png",
     "SpeedFactor": 1.88,
-    "BaseSlots": 8,
-    "BaseWeight": 900,
+    "Slots": 8,
+    "Weight": 900,
     "Id": 2
   },
   {
     "Name": "Pack Elephant",
     "Icon": "data/img/transport/elephant.png",
     "SpeedFactor": 1.37,
-    "BaseSlots": 7,
-    "BaseWeight": 1700,
+    "Slots": 7,
+    "Weight": 1700,
     "Id": 3
   }
 ]
